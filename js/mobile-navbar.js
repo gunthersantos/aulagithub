50a8cff4b405fa622b56e679610b1620253ef76f
Gunther Santos <gunthersantos@gmail.com>
2022-02-22 16:59:26 -0300

    Acrescentado fotos do carrossel

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -42,3 +42,31 @@ const mobileNavbar = new MobileNavbar(
   ".nav-list li"
 );
 mobileNavbar.init();
+
+// Carrossel de Imagens
+// Instantiate the Bootstrap carousel
+$(".multi-item-carousel").carousel({
+  interval: false,
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$(".multi-item-carousel .item").each(function () {
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(":first");
+  }
+  next.children(":first-child").clone().appendTo($(this));
+
+  if (next.next().length > 0) {
+    next.next().children(":first-child").clone().appendTo($(this));
+  } else {
+    $(this)
+      .siblings(":first")
+      .children(":first-child")
+      .clone()
+      .appendTo($(this));
+  }
+});
+
+// Fim do Carrossel de Imagens
